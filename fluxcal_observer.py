9a5a80e21434d36a985f1751c71417ff533313c1
itoledoc <ignacio.toledo@alma.cl>
2020-03-08 14:54:13 -0300

    FluxcalObs class improved.

--- fluxcal_observer.py
+++ fluxcal_observer.py
@@ -15,32 +15,33 @@ class FluxcalObs(object):
 
         self.sun = get_sso_coordinates('Sun', self.epoch)
         self.moon = get_sso_coordinates('Moon', self.epoch)
-        self._io = get_sso_coordinates('Io', self.epoch)
-        self._europa = get_sso_coordinates('Europa', self.epoch)
-        self._ganymede = get_sso_coordinates('Ganymede', self.epoch)
-        self._callisto = get_sso_coordinates('Callisto', self.epoch)
-        self._jupiter = get_sso_coordinates('Jupiter', self.epoch)
-
-        self.ganymede = self.create_source(
-            'Ganymede', skycoord_dict={
-                'Jupiter': self._jupiter, 'Io': self._io,
-                'Europa': self._europa, 'Callisto': self._callisto,
-                'Sun': self.sun, 'Moon': self.moon})
-        self.callisto = self.create_source(
-            'Callisto', skycoord_dict={
-                'Jupiter': self._jupiter, 'Io': self._io,
-                'Europa': self._europa, 'Ganymede': self._ganymede,
-                'Sun': self.sun, 'Moon': self.moon})
-
         self._steps = len(self.sun.obstime)
 
-        self.mars = self.create_source('Mars')
-        self.uranus = self.create_source('Uranus')
-        self.neptune = self.create_source('Neptune')
+        self.main_frame = pd.concat(
+            [self._create_source('Mars'), self._create_source('Uranus'),
+             self._create_source('Neptune')], axis=0, sort=False)
+        self.sources = ['Mars', 'Uranus', 'Neptune']
 
-    def create_source(
+        self._io_skycoord = get_sso_coordinates('Io', self.epoch)
+        self._europa_skycoord = get_sso_coordinates('Europa', self.epoch)
+        self._ganymede_skycoord = get_sso_coordinates('Ganymede', self.epoch)
+        self._callisto_skycoord = get_sso_coordinates('Callisto', self.epoch)
+        self._jupiter_skycoord = get_sso_coordinates('Jupiter', self.epoch)
+
+        self.add_source(
+            'Ganymede', skycoord_dict={
+                'Jupiter': self._jupiter_skycoord, 'Io': self._io_skycoord,
+                'Europa': self._europa_skycoord, 'Callisto': self._callisto_skycoord,
+                'Sun': self.sun, 'Moon': self.moon})
+        self.add_source(
+            'Callisto', skycoord_dict={
+                'Jupiter': self._jupiter_skycoord, 'Io': self._io_skycoord,
+                'Europa': self._europa_skycoord, 'Ganymede': self._ganymede_skycoord,
+                'Sun': self.sun, 'Moon': self.moon})
+
+    def _create_source(
             self, name: str, ra: Angle = None, dec: Angle = None,
-            skycoord_dict: Dict[str, SkyCoord] = None
+            skycoord_dict: Dict[str, SkyCoord] = None, debug: bool = False
     ) -> pd.DataFrame:
         """
 
@@ -63,18 +64,24 @@ class FluxcalObs(object):
             coords = SkyCoord(ra, dec, frame='icrs', obstime=obstime)
 
         if skycoord_dict:
-            df = self.build_dataframe(
+            df = self._build_dataframe(
                 coords, skycoord_dict)
         else:
-            df = self.build_dataframe(
+            df = self._build_dataframe(
                 coords, {'Sun': self.sun, 'Moon': self.moon})
 
         self._prepare_coords_df(df, coords)
 
-        return df
+        df['source'] = name
+        if debug:
+            return df
+        else:
+            cols = ['timestamp', 'source', 'lst', 'ha', 'closest_object',
+                    'closest_distance', 'altitude', 'ha']
+            return df[cols]
 
     @staticmethod
-    def build_dataframe(coords, skycoord_dict):
+    def _build_dataframe(coords, skycoord_dict):
         """
 
         :param coords::
@@ -131,10 +138,18 @@ class FluxcalObs(object):
             lambda x: calc_ha(x['ra'], x['lst']), axis=1)
         df.drop(['ra'], axis=1, inplace=True)
 
-    @staticmethod
-    def cal_selector(
-            df: pd.DataFrame, horizon: float = 20.,
-            sun_limit: float = 21., moon_limit: float = 5.
+    def add_source(
+            self, name: str, ra: Angle = None, dec: Angle = None,
+            skycoord_dict: Dict[str, SkyCoord] = None):
+
+        new_source = self._create_source(name, ra, dec, skycoord_dict)
+
+        self.main_frame = pd.concat([self.main_frame, new_source])
+        self.sources.append(name)
+
+    def apply_selector(
+            self, horizon: float = 20., sun_limit: float = 21.,
+            moon_limit: float = 5.
     ) -> None:
 
         """
@@ -147,11 +162,14 @@ class FluxcalObs(object):
         :return:
         """
 
-        df['selAlt'] = df.altitude.apply(lambda x: x >= horizon)
-        df['selSun'] = df.Sun.apply(lambda x: x >= sun_limit * 3600)
-        df['selMoon'] = df.Moon.apply(lambda x: x >= moon_limit * 3600)
+        self.main_frame['selAlt'] = self.main_frame.altitude.apply(
+            lambda x: x >= horizon)
+        self.main_frame['selSun'] = self.main_frame.Sun.apply(
+            lambda x: x >= sun_limit * 3600)
+        self.main_frame['selMoon'] = self.main_frame.Moon.apply(
+            lambda x: x >= moon_limit * 3600)
 
-        band_columns = ['Band3', 'Band6', 'Band7','Band9']
+        band_columns = ['Band3', 'Band6', 'Band7', 'Band9']
 
-        df[band_columns] = df.closest_distance.apply(
+        self.main_frame[band_columns] = self.main_frame.closest_distance.apply(
             lambda x: band_limits(x))
