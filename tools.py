a16fbacb77af9f76413151fec9b9fbe3ae5c6e84
itoledoc <ignacio.toledo@alma.cl>
2020-03-08 13:38:51 -0300

    tools.py created. Auxiliary methods for the fluxcal_observer library moved here.

---
+++ tools.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+from typing import Union, Dict
+from astropy import units as u
+from astropy.table import Table
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.coordinates.angles import Angle
+from astroquery.jplhorizons import Horizons, Conf
+from astropy.time import Time
+
+ALMA = EarthLocation(
+    lat=-23.0262015 * u.deg,
+    lon=-67.7551257 * u.deg,
+    height=5060 * u.m)
+
+ALMA_COORD = {
+    'lon': ALMA.lon.degree,
+    'lat': ALMA.lat.degree,
+    'elevation': ALMA.height.to_value(unit='km')}
+
+# Add new ephemeris columns that are being returned by the JPL service.
+# Workaround.
+Conf.eph_columns['ObsSub-LON'] = ('a1', 'deg')
+Conf.eph_columns['ObsSub-LAT'] = ('a2', 'deg')
+Conf.eph_columns['SunSub-LON'] = ('a3', 'deg')
+Conf.eph_columns['SunSub-LAT'] = ('a4', 'deg')
+
+SSO_ID_DICT = {
+    'Sun': 0, 'Moon': 301, 'Mars': 4,
+    'Jupiter': 5, 'Uranus': 7, 'Neptune': 8,
+    'Io': 501, 'Europa': 502, 'Ganymede': 503,
+    'Callisto': 504
+}
+
+BAND_LIMS = {'B3': 217.45,
+             'B6': 90.99,
+             'B7': 61.72,
+             'B9': 31.46}
+
+JUP_RADIUS = 25.
+
+
+def calc_ha(ra: float, lst: float) -> float:
+    """
+      Returns the Hour Angle of an RA coordinate given an LST
+
+    :param ra: Right ascension in degrees
+    :param lst: Local sideral time in decimal hours
+    :return: hour angle, in hours.
+    """
+
+    ha = np.degrees(
+        np.math.atan2(
+            np.sin(np.deg2rad(lst*15.) - np.deg2rad(ra)),
+            np.cos(np.deg2rad(lst*15.) - np.deg2rad(ra))
+        )
+    )/15.
+
+    return ha
+
+
+def get_sso_coordinates(
+        sso_name: str, epoch: Union[float, Dict[str, str]],
+        raw_table: bool = False
+) -> Union[SkyCoord, Table]:
+
+    """
+      Get the ICRS coordinates of a Solar System Object from JPL Horizons as a
+    SkyCord object. For debuging purposes, or further information, is possible
+    to get a Table object with the all the information from Horizons.
+
+    :param epoch: jd time or epochs dict
+    :param sso_name: str. Must be any of Sun, Moon, Mars, Jupter, Uranus,
+        Neptune
+    :param raw_table:
+    :return: SkyCoord object with the SSO coordinates at the given time
+    """
+
+    if sso_name not in SSO_ID_DICT.keys():
+        raise KeyError('SSO Name provided is not valid.')
+
+    source_query = Horizons(
+        id=SSO_ID_DICT[sso_name], location=ALMA_COORD, epochs=epoch,
+        id_type='id'
+    )
+    source_table = source_query.ephemerides()
+
+    if raw_table:
+        return source_table
+
+    source = SkyCoord(
+        ra=source_table['RA'].tolist() * u.deg,
+        dec=source_table['DEC'].tolist() * u.deg,
+        frame='icrs', obstime=Time(source_table['datetime_jd'], format='jd'))
+    return source
+
+
+def band_limits(x: float) -> pd.Series:
+
+    band3, band6, band7, band9 = False, False, False, False
+
+    if x > BAND_LIMS['B3']:
+        band3, band6, band7, band9 = True, True, True, True
+    elif x > BAND_LIMS['B6']:
+        band6, band7, band9 = True, True, True
+    elif x > BAND_LIMS['B7']:
+        band7, band9 = True, True
+    elif x > BAND_LIMS['B9']:
+        band9 = True
+
+    return pd.Series([band3, band6, band7, band9])
+
