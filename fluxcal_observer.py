81851754371f9a04eb3123e4ec6d3583ff580b29
itoledoc <ignacio.toledo@alma.cl>
2020-03-25 11:10:13 -0300

    Working on documentation

--- fluxcal_observer.py
+++ fluxcal_observer.py
@@ -1,28 +1,70 @@
-from tools import *
+import numpy as np
+import pandas as pd
+
+from typing import Union, Dict
+from astropy import units as u
+from astropy.table import Table
+from astropy.coordinates import SkyCoord, EarthLocation
+from astroquery.jplhorizons import Horizons, Conf
+from astropy.time import Time
+
 from astropy.coordinates.angles import Angle
 from astropy.coordinates import AltAz
 from astropy.utils import iers
 
+
 iers.conf.auto_download = True
 iers.conf.remote_timeout = 40
 
+ALMA = EarthLocation(
+    lat=-23.0262015 * u.deg,
+    lon=-67.7551257 * u.deg,
+    height=5060 * u.m)
 
-class FluxcalObs:
+ALMA_COORD = {
+    'lon': ALMA.lon.degree,
+    'lat': ALMA.lat.degree,
+    'elevation': ALMA.height.to_value(unit='km')}
+
+# Add new ephemeris columns that are being returned by the JPL service.
+# Workaround.
+Conf.eph_columns['ObsSub-LON'] = ('a1', 'deg')
+Conf.eph_columns['ObsSub-LAT'] = ('a2', 'deg')
+Conf.eph_columns['SunSub-LON'] = ('a3', 'deg')
+Conf.eph_columns['SunSub-LAT'] = ('a4', 'deg')
+
+SSO_ID_DICT = {
+    'Sun': 0, 'Moon': 301, 'Mars': 4,
+    'Jupiter': 5, 'Uranus': 7, 'Neptune': 8,
+    'Io': 501, 'Europa': 502, 'Ganymede': 503,
+    'Callisto': 504
+}
+
+BAND_LIMS = {'B3': 217.45,
+             'B6': 90.99,
+             'B7': 61.72,
+             'B9': 31.46}
+
+JUP_RADIUS = 25.
+
+
+class FluxcalObs(object):
+    """ Class to handle the observability of Calibrators that need monitoring.
+
+    Parameters
+    ----------
+    start : string
+        A start date in format 'YYYY-MM-DD'.
+    stop : string
+        An end date, Same format as start
+    step : string
+        Optional, every how much time do we information (specially relevant for
+        ephemeris objects)
+    """
 
     def __init__(self, start: str, stop: str, step: str = '15min'):
-        """
-          Class to handle the observability of Calibrators that need monitoring.
 
-        :param start: a start date in format 'YYYY-MM-DD', as string.
-        :param stop: an end date, string. Same format
-        :param step: optional, every how much time do we information (specially
-          relevant for ephemeris objects)
-
-        Retunrs an instance
-
-        """
         self.epoch = {'start': start, 'stop': stop, 'step': step}
-
         self.sun = get_sso_coordinates('Sun', self.epoch)
         self.moon = get_sso_coordinates('Moon', self.epoch)
         self._steps = len(self.sun.obstime)
@@ -54,7 +96,7 @@ class FluxcalObs:
     def _create_source(
             self, name: str, ra: Angle = None, dec: Angle = None,
             skycoord_dict: Dict[str, SkyCoord] = None, debug: bool = False
-        ) -> pd.DataFrame:
+            ) -> pd.DataFrame:
         """
           Auxiliary method to create sources given a name, coordinates, and a
         dictionary. The dictionary contains SkyCoord objects for the reference
@@ -194,3 +236,74 @@ class FluxcalObs:
 
         self.main_frame[band_columns] = self.main_frame.closest_distance.apply(
             lambda x: band_limits(x))
+
+
+def calc_ha(ra: float, lst: float) -> float:
+    """
+      Returns the Hour Angle of an RA coordinate given an LST
+
+    :param ra: Right ascension in degrees
+    :param lst: Local sideral time in decimal hours
+    :return: hour angle, in hours.
+    """
+
+    ha = np.degrees(
+        np.math.atan2(
+            np.sin(np.deg2rad(lst*15.) - np.deg2rad(ra)),
+            np.cos(np.deg2rad(lst*15.) - np.deg2rad(ra))
+        )
+    )/15.
+
+    return ha
+
+
+def get_sso_coordinates(
+        sso_name: str, epoch: Union[float, Dict[str, str]],
+        raw_table: bool = False
+        ) -> Union[SkyCoord, Table]:
+
+    """
+      Get the ICRS coordinates of a Solar System Object from JPL Horizons as a
+    SkyCord object. For debuging purposes, or further information, is possible
+    to get a Table object with the all the information from Horizons.
+
+    :param epoch: jd time or epochs dict
+    :param sso_name: str. Must be any of Sun, Moon, Mars, Jupter, Uranus,
+       Neptune
+    :param raw_table:
+    :return: SkyCoord object with the SSO coordinates at the given time
+    """
+
+    if sso_name not in SSO_ID_DICT.keys():
+        raise KeyError('SSO Name provided is not valid.')
+
+    source_query = Horizons(
+        id=SSO_ID_DICT[sso_name], location=ALMA_COORD, epochs=epoch,
+        id_type='id'
+    )
+    source_table = source_query.ephemerides()
+
+    if raw_table:
+        return source_table
+
+    source = SkyCoord(
+        ra=source_table['RA'].tolist() * u.deg,
+        dec=source_table['DEC'].tolist() * u.deg,
+        frame='icrs', obstime=Time(source_table['datetime_jd'], format='jd'))
+    return source
+
+
+def band_limits(x: float) -> pd.Series:
+
+    band3, band6, band7, band9 = False, False, False, False
+
+    if x > BAND_LIMS['B3']:
+        band3, band6, band7, band9 = True, True, True, True
+    elif x > BAND_LIMS['B6']:
+        band6, band7, band9 = True, True, True
+    elif x > BAND_LIMS['B7']:
+        band7, band9 = True, True
+    elif x > BAND_LIMS['B9']:
+        band9 = True
+
+    return pd.Series([band3, band6, band7, band9])
